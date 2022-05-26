94e87b859c0d12c440f9d281c9cec9316ece8a3b
inal <78749120+inalgarayev@users.noreply.github.com>
2022-05-26 08:15:41 -0700

    node

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const bodyparser = require("body-parser");
-const path = require('path');
+const express = require('express')
+const app = express()
+const { ROLE, users } = require('./data')
+const { authUser, authRole } = require('./basicAuth')
+const projectRouter = require('./routes/projects')
 
-const connectDB = require('./server/database/connection');
+app.use(express.json())
+app.use(setUser)
+app.use('/projects', projectRouter)
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Home Page')
+})
 
-dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT || 8080
+app.get('/dashboard', authUser, (req, res) => {
+  res.send('Dashboard Page')
+})
 
-// log requests
-app.use(morgan('tiny'));
+app.get('/admin', authUser, authRole(ROLE.ADMIN), (req, res) => {
+  res.send('Admin Page')
+})
 
-// mongodb connection
-connectDB();
+function setUser(req, res, next) {
+  const userId = req.body.userId
+  if (userId) {
+    req.user = users.find(user => user.id === userId)
+  }
+  next()
+}
 
-// parse request to body-parser
-app.use(bodyparser.urlencoded({ extended : true}))
-
-// set view engine
-app.set("view engine", "ejs")
-//app.set("views", path.resolve(__dirname, "views/ejs"))
-
-// load assets
-app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
-app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
-app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
-
-// load routers
-app.use('/', require('./server/routes/router'))
-
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(3000)
